c0958646707e8f068522f67f0c6ef2dabcfa4de8
Alex Pereira <alex.pereira@outlook.com>
2017-11-29 14:21:49 -0500

    Add security group to allow incoming and outcoming traffic to instance

--- deploy-a-single-web-server/main.tf
+++ deploy-a-single-web-server/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "single-web-server" {
     ami = "ami-40d28157"
     instance_type = "t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
     tags {
         Name = "single-web-server"
@@ -15,4 +16,15 @@ resource "aws_instance" "single-web-server" {
                 echo "Welcome to my web page :)" > index.html
                 nohup busybox httpd -f -p 8080 &
                 EOF
+}
+
+resource "aws_security_group" "instance" {
+    name = "single-web-page-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
