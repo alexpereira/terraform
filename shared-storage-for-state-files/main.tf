00eb88b6324b1e2587d211f8c3f536967d80d3cb
Alex Pereira <alex.pereira@outlook.com>
2017-12-01 10:01:49 -0500

    Comment terraform backend in first step - bucket needs to exist

--- shared-storage-for-state-files/main.tf
+++ shared-storage-for-state-files/main.tf
@@ -14,14 +14,14 @@ resource "aws_s3_bucket" "cool-app-state" {
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket  = "cool-app-state-yolo"
-    key     = "terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket  = "cool-app-state-yolo"
+#     key     = "terraform.tfstate"
+#     region  = "us-east-1"
+#     encrypt = true
+#   }
+# }
 
 output "s3_bucket_arn" {
   value = "${aws_s3_bucket.cool-app-state.arn}"
